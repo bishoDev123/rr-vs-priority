ef8193cd0d7be45dd652c8d3eb6fc1c99e96e5ae
bishoDev123 <bisholiveenglish@gmail.com>
2026-04-28 22:36:30 +0300

    added instructions in priority.js

--- js/priority/priority.js
+++ js/priority/priority.js
@@ -1,6 +1,8 @@
 import ExecutionTrace from "../shared/models"
 
-// DO NOT TOUCH
+// input: processes (with id and priority)
 function priority(processes) {
     return new ExecutionTrace(1, 0, 4)
 }
+
+// output: execution trace with id, start time, and finish time
