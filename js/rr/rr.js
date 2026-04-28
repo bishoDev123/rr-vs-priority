0fcd9ff245fabdcee41862f972251cf81d4862f5
bishoDev123 <bisholiveenglish@gmail.com>
2026-04-28 22:34:06 +0300

    added instructions in rr.js

--- js/rr/rr.js
+++ js/rr/rr.js
@@ -1,6 +1,7 @@
 import ExecutionTrace from "../shared/models"
 
-// DO NOT TOUCH
+// input: processes (they will have an id and priority, you should ignore the priority), quantum time
 function roundRobin(processes, quantum) {
     return new ExecutionTrace(1, 0, 4)
 }
+// output: Execution trace with id, start time, and finish time
